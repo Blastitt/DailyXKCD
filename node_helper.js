3ad2d84095d16bab5e7a668e9978d4b06dae68f5
xander <xander.no@gmail.com>
2016-10-21 07:11:41 +0200

    Randomizer and size restraint

--- node_helper.js
+++ node_helper.js
@@ -15,15 +15,34 @@ module.exports = NodeHelper.create({
 		if(notification === "GET_COMIC"){
 			
 			var comicJsonUri = payload.config.dailyJsonUrl;
+			var comic;
+			var rndcomic;
+			var rndUrl;
+			var body;
+			var d = new Date();
+			var n = d.getDay(); 
 			
 			request(comicJsonUri, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
-					console.log(body);
-					self.sendSocketNotification("COMIC", JSON.parse(body));
-					console.log(JSON.parse(body).img);
-					
+					//console.log(body);
+					if ( this.n == 1 || this.n == 3 || this.n == 5 ) {
+							self.sendSocketNotification("COMIC", JSON.parse(body));
+					} else {
+						this.comic = JSON.parse(body); 
+						this.rndcomic = Math.floor((Math.random() * this.comic.num) + 1); 
+						this.rndUrl = "http://xkcd.com/" + this.rndcomic + "/info.0.json";
+						request(this.rndUrl, function (error, response, body) {
+							if (!error && response.statusCode == 200) {
+								//console.log(body);
+								self.sendSocketNotification("COMIC", JSON.parse(body));
+							}
+						});
+					}
 				}
 			});
+
+
+
 			
 		}
 		
